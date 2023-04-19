c45735a7242e5c01ea1ba2929e0f59cb7b92b39c
semkwon <3pkwon@gmail.com>
2023-04-19 21:02:18 +0900

    start button

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-blue-200 flex justify-center items-center">
+      <button className="bg-purple-300 px-4 py-2 border-black border-2 rounded-lg hover:bg-purple-500 hover:border-4 hover:text-lg hover:font-semibold">
+        🐥 Start Flappy Bird!!! 🐥
+      </button>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
